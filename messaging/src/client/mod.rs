216eae0ff5ead2ba14474a49522beedd17b63a41
Tomás Cichero <martoxdlol@gmail.com>
2024-04-24 19:52:02 +0000

    Aplicar cargo clippy --fix

--- messaging/src/client/mod.rs
+++ messaging/src/client/mod.rs
@@ -1,5 +1,11 @@
 pub struct NATSClient {}
 
+impl Default for NATSClient {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl NATSClient {
     pub fn new() -> Self {
         NATSClient {}
