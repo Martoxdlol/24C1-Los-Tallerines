3d073b992bb470789c0540f4e4a01457024e748a
rodoAlbornoz <ralbornozt@fi.uba.ar>
2024-05-11 03:07:53 +0000

    Añado archivo de los drones para dibujarlos en el mapa

---
+++ monitoring/src/dron.rs
@@ -0,0 +1,20 @@
+use walkers::{extras::Style, Position};
+
+pub struct Dron {
+    pub posicion: Position,
+    pub nombre: String,
+    pub icono: char,
+    pub estilo: Style,
+    pub radio: f64,
+}
+impl Dron {
+    pub fn new(longitud: f64, latitud: f64, nombre: String) -> Self {
+        Dron {
+            posicion: Position::from_lon_lat(longitud, latitud),
+            nombre,
+            icono: '🚁',
+            estilo: Style::default(),
+            radio: 150.,
+        }
+    }
+}
