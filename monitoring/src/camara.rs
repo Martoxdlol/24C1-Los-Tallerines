c1b3d57489670e1f47245c1e650fd8f6075e630a
Valeria Brzoza <valeriabrzoza@gmail.com>
2024-05-13 15:47:48 -0300

    Sombreado con zoom

--- monitoring/src/camara.rs
+++ monitoring/src/camara.rs
@@ -2,16 +2,16 @@ use walkers::{extras::Style, Position};
 
 pub struct Camara {
     pub posicion: Position,
-    pub nombre: String,
+    pub id: u64,
     pub icono: char,
     pub estilo: Style,
     pub radio: f64,
 }
 impl Camara {
-    pub fn new(longitud: f64, latitud: f64, nombre: String) -> Self {
+    pub fn new(longitud: f64, latitud: f64, id: u64) -> Self {
         Camara {
             posicion: Position::from_lon_lat(longitud, latitud),
-            nombre,
+            id,
             icono: '📹',
             estilo: Style::default(),
             radio: 150.,
