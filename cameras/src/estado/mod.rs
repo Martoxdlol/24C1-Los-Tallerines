aef741fd3f74eae414e1a009557f5f64031d72c5
Valeria Brzoza <valeriabrzoza@gmail.com>
2024-05-15 18:09:53 -0300

    Documentación estado

--- cameras/src/estado/mod.rs
+++ cameras/src/estado/mod.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 
 use crate::camara::{id::IdCamara, Camara};
 
+/// Estado del sistema entero. Registra todo.
+/// TODO: Estado de incidentes y de drones
 pub struct Estado {
     camaras: HashMap<IdCamara, Camara>,
 }
@@ -19,6 +21,8 @@ impl Estado {
         }
     }
 
+    /// Al conectar una cámara, se registra.
+    /// Todas las cámaras deben tener IDs únicos.
     pub fn conectar_camara(&mut self, camara: Camara) -> Result<(), String> {
         if self.camaras.contains_key(&camara.id) {
             return Err("Ya existe una cámara con ese ID".to_string());
@@ -27,6 +31,8 @@ impl Estado {
         Ok(())
     }
 
+    /// Al desconectar una cámara, se borra.
+    /// La cámara debe estar conectada para poder desconectarse.
     pub fn desconectar_camara(&mut self, id: IdCamara) -> Result<(), String> {
         if !self.camaras.contains_key(&id) {
             return Err("No existe una cámara con ese ID".to_string());
@@ -35,13 +41,18 @@ impl Estado {
         Ok(())
     }
 
+    /// Encuentra una cámara por su ID.
     pub fn camara(&self, id: IdCamara) -> Option<&Camara> {
         self.camaras.get(&id)
     }
 
+    /// Lista todas las cámaras conectadas.
     pub fn camaras(&self) -> Vec<&Camara> {
         self.camaras.values().collect()
     }
+
+    /// Modifica la ubicación de una cámara ya registrada.
+    /// TODO: ESTA BIEN ESTO????????
     pub fn modificar_ubicacion(&mut self, id: IdCamara, lat: f64, lon: f64) -> Result<(), String> {
         match self.camaras.get_mut(&id) {
             Some(camara) => {
@@ -53,6 +64,7 @@ impl Estado {
         }
     }
 
+    /// Modifica el rango de una cámara ya registrada.
     pub fn modificar_rango(&mut self, id: IdCamara, rango: f64) -> Result<(), String> {
         match self.camaras.get_mut(&id) {
             Some(camara) => {
