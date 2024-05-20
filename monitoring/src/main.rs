0975439bc34809513e05cad093887f5766feac40
Tomás Cichero <martoxdlol@gmail.com>
2024-05-20 12:38:41 -0300

    usar egui en el thread principal

--- monitoring/src/main.rs
+++ monitoring/src/main.rs
@@ -1,30 +1,26 @@
 use monitoring::{logica::intentar_iniciar_sistema, Aplicacion};
 use std::{sync::mpsc::channel, thread};
 
-fn main() {
+fn main() -> Result<(), eframe::Error> {
     let (enviar_comando, recibir_comando) = channel();
     let (enviar_estado, recibir_estado) = channel();
 
     thread::spawn(move || {
-        if eframe::run_native(
-            "APLICACION DE MONITOREO", // Nombre de la ventana
-            Default::default(),
-            Box::new(|cc| {
-                Box::new(Aplicacion::new(
-                    enviar_comando,
-                    recibir_estado,
-                    cc.egui_ctx.clone(),
-                ))
-            }),
-        )
-        .is_err()
-        {
+        if let Err(e) = intentar_iniciar_sistema(recibir_comando, enviar_estado) {
+            eprintln!("Error al iniciar el sistema: {}", e);
             std::process::exit(1);
         }
     });
 
-    if let Err(e) = intentar_iniciar_sistema(recibir_comando, enviar_estado) {
-        eprintln!("Error al iniciar el sistema: {}", e);
-        std::process::exit(1);
-    }
+    eframe::run_native(
+        "APLICACION DE MONITOREO", // Nombre de la ventana
+        Default::default(),
+        Box::new(|cc| {
+            Box::new(Aplicacion::new(
+                enviar_comando,
+                recibir_estado,
+                cc.egui_ctx.clone(),
+            ))
+        }),
+    )
 }
