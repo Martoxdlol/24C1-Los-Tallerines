4ca0f4a1ea01fddc8248d2129fc835581246435b
Tomás Cichero <martoxdlol@gmail.com>
2024-05-25 12:17:20 -0300

    fmt

--- monitoring/src/logica/estado.rs
+++ monitoring/src/logica/estado.rs
@@ -38,9 +38,9 @@ impl Estado {
     }
 
     pub fn incidentes(&self) -> Vec<Incidente> {
-       let mut v: Vec<Incidente>= self.incidentes.values().cloned().collect();
-       v.sort_by(|a,b| b.inicio.cmp(&a.inicio));
-       v
+        let mut v: Vec<Incidente> = self.incidentes.values().cloned().collect();
+        v.sort_by(|a, b| b.inicio.cmp(&a.inicio));
+        v
     }
 
     pub fn camaras(&self) -> Vec<Camara> {
