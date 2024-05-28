03771b176114154e9f7917dcba4ab02b8fd68b81
RodoAlbornoz <ralbornozt@fi.uba.ar>
2024-05-28 20:04:24 -0300

    Crear archivo de drones al cargar dron

--- drone/src/lib.rs
+++ drone/src/lib.rs
@@ -5,7 +5,6 @@ pub struct AplicacionDron {
     configuracion: Configuracion,
 }
 
-
 impl AplicacionDron {
 
     pub fn new(configuracion: Configuracion) -> Self {
@@ -13,6 +12,23 @@ impl AplicacionDron {
     } 
 
     pub fn iniciar(&mut self) -> io::Result<()> {
+        self.cargar_dron()?;
+
+
+        Ok(())
+    }
+
+    fn cargar_dron(&mut self) -> io::Result<()> {
+        let ruta_archivo_drones = self
+            .configuracion
+            .obtener::<String>("drones")
+            .unwrap_or("drones.csv".to_string());
+
+        let existe = std::path::Path::new(&ruta_archivo_drones).exists();
+
+        if !existe {
+            std::fs::File::create(&ruta_archivo_drones)?;
+        }
 
         Ok(())
     }
