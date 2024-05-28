08d60cc32670b98ba0071cf91e3ab4896d699044
RodoAlbornoz <ralbornozt@fi.uba.ar>
2024-05-28 16:14:22 -0300

    Crear archivos de lib, sistema, estado, interfaz

--- drone/src/main.rs
+++ drone/src/main.rs
@@ -1,3 +1,6 @@
+use drone::{estado::Estado, interfaz::interfaz, sistema::Sistema};
+use lib::configuracion::Configuracion;
+
 fn main() {
     if let Err(e) = intentar_iniciar_sistema() {
         eprintln!("Error al iniciar el sistema: {}", e);
