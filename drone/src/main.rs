21b7bacbde5e61f87c47d26f33daf75099cf60aa
Tomás Cichero <martoxdlol@gmail.com>
2024-06-06 15:58:26 -0300

    avance con drones

--- drone/src/main.rs
+++ drone/src/main.rs
@@ -1,5 +1,58 @@
 // use messaging::client::NATSClient;
+use chrono::prelude::*;
+use lib::coordenadas::Coordenadas;
 
 fn main() {
-    // let client = NATSClient::new();
+
+    let mut bateria = 100.;
+
+    let mut ms_ultima_iteracion = chrono::offset::Local::now().timestamp_millis();
+
+    let mut rapidez = 2.5; // m/s
+
+    let mut direccion = 155.; // grados
+
+    let mut posicion = Coordenadas::from_lat_lon(-34.6037, -58.3816);
+
+    let mut destino = Coordenadas::from_lat_lon(-34.6020, -58.3689);
+
+    let descarga_bateria = 1./3600.;
+
+    loop {
+        let ms_ahora = chrono::offset::Local::now().timestamp_millis();
+        let diferencial_tiempo = ms_ahora - ms_ultima_iteracion;
+
+        let movimiento = rapidez * diferencial_tiempo as f64;
+
+        posicion = posicion.mover_en_direccion(movimiento, direccion);
+
+        let distancia_a_destino = posicion.distancia(&destino);
+
+        bateria -= descarga_bateria;
+
+
+        if bateria < 10. {
+            destino = Coordenadas::from_lat_lon(-34.6040, -58.3619);
+        }
+
+
+        if distancia_a_destino < 1. {
+            // Llegamos al destino yey!
+
+            // si destino es estación de carga, cargar bateria
+
+            rapidez = 0.;
+        }
+
+        if distancia_a_destino > 1. {
+            // Ajustar direccion
+            let diff_lat = destino.lat - posicion.lat;
+            let diff_lon = destino.lon - posicion.lon;
+            let hipotenusa = (diff_lat.powi(2) + diff_lon.powi(2)).sqrt();
+            let direccion = (diff_lat / hipotenusa).acos().to_degrees();
+        }
+        
+
+        std::thread::sleep(std::time::Duration::from_millis(20));
+    }
 }
--- lib/src/coordenadas/mod.rs
+++ lib/src/coordenadas/mod.rs
@@ -5,7 +5,7 @@ pub struct Coordenadas {
 }
 
 impl Coordenadas {
-    // In metters
+    // En metros
     pub fn distancia(&self, other: &Self) -> f64 {
         let d_lat = (self.lat - other.lat).to_radians();
         let d_lon = (self.lon - other.lon).to_radians();
@@ -20,6 +20,23 @@ impl Coordenadas {
 
     pub fn from_lat_lon(lat: f64, lon: f64) -> Self {
         Coordenadas { lat, lon }
+    }
+
+    pub fn mover_en_direccion(&self, distancia: f64, direccion: f64) -> Self { 
+        let metros_lat = distancia * ((direccion).to_radians()).cos();
+        let metros_lon = distancia * ((direccion).to_radians()).sin();
+
+        self.mover(metros_lat, metros_lon)
+    }
+
+    pub fn mover(&self, metros_lat: f64, metros_lon: f64) -> Self {
+        let metros_por_grado = self.distancia(&Self::from_lat_lon(self.lat+1., self.lon));
+        let grados_por_metro = 1. / metros_por_grado;
+
+        Coordenadas {
+            lat: self.lat + metros_lat * grados_por_metro,
+            lon: self.lon + metros_lon * grados_por_metro,
+        }
     }
 }
 
@@ -36,5 +53,27 @@ mod tests {
 
         assert!(distancia > 1170.);
         assert!(distancia < 1190.);
+    }
+
+    #[test]
+    fn mover() {
+        let obelisco = Coordenadas::from_lat_lon(-34.6037, -58.3816);
+        let destino = obelisco.mover(10000., 0.);
+
+        println!("{:?}", destino);
+        assert!(destino.lat > -34.5138);
+        assert!(destino.lat < -34.5137);   
+        assert!(destino.lon == -58.3816);
+    }
+
+    #[test]
+    fn mover_en_direccion() {
+        let obelisco = Coordenadas::from_lat_lon(-34.6037, -58.3816);
+        let destino = obelisco.mover_en_direccion(10000., 155.);
+
+        println!("{:?}", destino);
+        assert!(destino.lat > -34.5138);
+        assert!(destino.lat < -34.5137);   
+        assert!(destino.lon == -58.3689);
     }
 }
