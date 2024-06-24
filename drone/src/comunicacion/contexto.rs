d3f070d38826a03729a336e7f4aa6a3854f39e69
Tomás Cichero <martoxdlol@gmail.com>
2024-06-24 15:25:02 -0300

    drones + js = <3

--- drone/src/comunicacion/contexto.rs
+++ drone/src/comunicacion/contexto.rs
@@ -1,7 +1,9 @@
-use messaging_client::cliente::{suscripcion::Suscripcion, Cliente};
+use messaging_client::cliente::{
+    jetstream::js_suscripcion::JSSuscripcion, suscripcion::Suscripcion, Cliente,
+};
 
 pub struct Contexto {
     pub cliente: Cliente,
     pub suscripcion_incidentes_finalizados: Suscripcion,
-    pub suscripcion_comandos: Suscripcion,
+    pub suscripcion_comandos: JSSuscripcion,
 }
