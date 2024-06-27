26dba57c13760962cc4fbef1bde52df253f1a1ac
Tomás Cichero <martoxdlol@gmail.com>
2024-06-27 15:00:55 -0300

    clasificación local de imagenes con tensor flow

---
+++ machine-learning/predict.py
@@ -0,0 +1,14 @@
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+
+model = load_model('incidentes.h5')
+
+img = image.load_img('./dataset/test/normal/non_fire.43.png', target_size=(150, 150))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+images = np.vstack([x])
+classes = model.predict(images, batch_size=10)
+
+print(classes)
